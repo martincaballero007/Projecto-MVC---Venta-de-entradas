201d6babd009fd668c78fd35e4b0b9ebc27bf7d8
Martin Caballero <martincaballero590@gmail.com>
2026-05-15 11:36:12 -0500

    Adicion Clase Tarjeta

---
+++ entradas/src/main/java/modelo/Tarjeta.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author odiol
+ */
+public class Tarjeta {
+
+    private int numero;
+    private String nombre;
+    private String fecha;
+    private int cvv;
+
+    public Tarjeta() {
+    }
+
+    public Tarjeta(int numero, String nombre, String fecha, int cvv) {
+        this.numero = numero;
+        this.nombre = nombre;
+        this.fecha = fecha;
+        this.cvv = cvv;
+    }
+
+    public boolean validar() {
+        return numero > 0 && cvv > 0 && nombre != null && !nombre.isEmpty();
+    }
+
+    // Getters y Setters
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(int cvv) {
+        this.cvv = cvv;
+    }
+}
